9ab9332a4fd42e7914fd47d5a80e8f26b638953d
Cody Young <codyjamesyoung@gmail.com>
2019-10-22 15:02:13 -0600

    all routes working

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import {Link} from 'react-router-dom'
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount = () => {
+    axios
+    .get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+    .then(results => {
+      this.setState({
+        students: results.data
+      })
+    })
   }
 
   render() {
+    let studentsToDisplay = this.state.students.map((element, index) => {
+      return <Link key={index} to={`/student/${element.id}`}>
+        <h3 >{element.first_name} {element.last_name}</h3>
+      </Link>
+      // <h3 key={index}>{element.last_name}</h3>
+    })
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
+        {studentsToDisplay}
 
       </div>
     )
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 export default class Student extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    this.state = {
+      studentInfo: {}
+    }
+  }
 
+  componentDidMount = () => {
+    Axios
+    .get(`http://localhost:3005/students/${this.props.match.params.id}`)
+    .then(results => {
+      this.setState({
+        studentInfo: results.data
+      })
+    })
   }
 
   render() {
+    let student = this.state.studentInfo
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>{student.first_name} {student.last_name}</h1>
+        <h3>Grade: {student.grade}</h3>
+        <h3>Email: {student.email} </h3>
+
       </div>
     )
   }
